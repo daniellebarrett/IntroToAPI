24844aa93c23c4796020b88986014f216bb3f0f2
daniellebarrett <daniellebarrett@alumni.iu.edu>
2021-01-19 18:53:32 -0500

    Intro to API

---
+++ IntroToAPI/Program.cs
@@ -0,0 +1,82 @@
+﻿using IntroToAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            //asynchronous returns tasks                                                        //.Result//tasks have to be completed as a second step
+            HttpResponseMessage response = httpClient.GetAsync("https://swapi.dev/api/people/1").Result;
+
+            if (response.IsSuccessStatusCode)  //dont need to set this to true bc it is already true as long as value is 200-299;otherwise false
+            {
+                /*var content = response.Content.ReadAsStringAsync().Result; //reading it as a string then converting it
+                var person = JsonConvert.DeserializeObject<Person>(content);*/
+
+                Person luke = response.Content.ReadAsAsync<Person>().Result;
+                Console.WriteLine(luke.Name);
+
+                foreach (string vehicleUrl in luke.Vehicles)
+                {
+                    HttpResponseMessage vehicleResponse = httpClient.GetAsync(vehicleUrl).Result;
+                    /* Console.WriteLine(vehicleResponse.Content.ReadAsStringAsync().Result);*/
+
+                    Vehicle vehicle = vehicleResponse.Content.ReadAsAsync<Vehicle>().Result;
+                    Console.WriteLine(vehicle.Name);
+                }
+            }
+
+            Console.WriteLine();
+
+            SWAPIService service = new SWAPIService();
+            Person person = service.GetPersonAsync("https://swapi.dev/api/people/11").Result;
+            if (person != null)
+            {
+                Console.WriteLine(person.Name);
+
+                foreach (var vehicleUrl in person.Vehicles)
+                {
+                    var vehicle = service.GetVehicleAsync(vehicleUrl).Result;
+                    Console.WriteLine(vehicle.Name);
+                }
+            }
+
+            Console.WriteLine();
+
+            var genericResponse = service.GetAsync<Vehicle>("https://swapi.dev/api/vehicles/4").Result;
+            //var genericResponse = service.GetAsync<Person>("https://swapi.dev/api/people/4").Result;
+            if (genericResponse != null)
+            {
+                Console.WriteLine(genericResponse.Name);
+            }
+            else
+            {
+                Console.WriteLine("Targeted object does not exist.");
+            }
+
+
+            Console.WriteLine();
+
+            //search for something
+            SearchResult<Person> skywalkers = service.GetPersonSearchAsync("skywalker").Result;
+            foreach(Person p in skywalkers.Results)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            //search for a vehicle 2 dif ways
+            var genericSearch = service.GetSearchAsync<Vehicle>("speeder", "vehicles").Result;
+            var vehicleSearch = service.GetVehicleSearchAsync("speeder").Result;
+        }
+    }
+}
---
+++ IntroToAPI/SWAPIService.cs
@@ -0,0 +1,84 @@
+﻿using IntroToAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToAPI
+{
+    class SWAPIService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+
+        // Async Method
+        public async Task<Person> GetPersonAsync(string url)
+        {
+            //// Get Request
+            //HttpResponseMessage response = await _httpClient.GetAsync(url);
+            //if (response.IsSuccessStatusCoded)
+            //{
+            //    // was a success
+            //    Person person = await response.Content.ReadAsAsync<Person>();
+            //    return person;
+            //}
+
+            //// was not a success
+            //return null;
+
+            return await GetAsync<Person>(url);
+
+        }
+
+        public async Task<Vehicle> GetVehicleAsync(string url)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            return response.IsSuccessStatusCode 
+                ? await response.Content.ReadAsAsync<Vehicle>()
+                : null;
+        }
+
+        public async Task<T> GetAsync<T>(string url)//where T:class -->says T has to be a class 100% of the time (cuz classes can be nullable, then can return null)
+            //constraints allow us to give more info about the generic
+            //constraints limit what programmers can do
+            //cant accidently misuse this method or else it will break - this is a result of using the constraint
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                T content = await response.Content.ReadAsAsync<T>();
+                return content;
+            }
+
+            //return null; this doesnt work bc T could be int and int is NEVER null? instead use default or use a constraint(line 42)
+            return default;
+        }
+
+        public async Task<SearchResult<Person>> GetPersonSearchAsync(string query)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("https://swapi.dev/api/people?search=" + query);
+
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadAsAsync<SearchResult<Person>>();
+            return null;
+        }
+
+        public async Task<SearchResult<T>> GetSearchAsync<T>(string query, string category)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"https://swapi.dev/api/{category}?search={query}");
+
+            return response.IsSuccessStatusCode
+                ? await response.Content.ReadAsAsync<SearchResult<T>>()
+                : default;
+        }
+
+        public async Task<SearchResult<Vehicle>> GetVehicleSearchAsync(string query)
+        {
+            return await GetSearchAsync<Vehicle>(query, "vehicles");
+        }
+    }
+}
